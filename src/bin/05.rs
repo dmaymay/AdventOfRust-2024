e205bcc8a290fe09593c120d5409181d6fb5217a
dmaymay <dmayoracurzio@gmail.com>
2024-12-10 01:06:41 +0100

    solved day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -74,7 +74,76 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-   None
+    let (rules, pages) = input.split_once("\n\n").unwrap();
+
+    let page_updates: Vec<Vec<u32>> = pages
+        .lines()
+        .map(|line| {
+            line.split(",")
+                .map(|n| n.trim().parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let mut page_rules = HashMap::<u32, PageRules>::new();
+
+    rules.lines().for_each(|line| {
+        let parts: Vec<&str> = line.split("|").collect();
+        let num1 = parts[0].parse::<u32>().unwrap();
+        let num2 = parts[1].parse::<u32>().unwrap();
+
+        page_rules
+            .entry(num1)
+            .or_insert(PageRules {
+                left: vec![],
+                right: vec![],
+            })
+            .right
+            .push(num2);
+
+        page_rules
+            .entry(num2)
+            .or_insert(PageRules {
+                left: vec![],
+                right: vec![],
+            })
+            .left
+            .push(num1);
+    });
+
+    let mut sum = 0;
+    let mut valid: bool = true;
+    page_updates.into_iter().for_each(|pages| {
+        valid = true;
+        for i in 0..pages.len() {
+            // Check numbers that should be before (left)
+            if let Some(rule) = page_rules.get(&pages[i]) {
+                if pages[i + 1..].iter().any(|&x| rule.left.contains(&x)) {
+                    valid = false;
+                    break;
+                }
+            }
+            // Check numbers that should be after (right)
+            if let Some(rule) = page_rules.get(&pages[i]) {
+                if pages[..i].iter().any(|&x| rule.right.contains(&x)) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        if !valid {
+            let mut pages = pages;
+            pages.sort_by(|a, b| {
+                if page_rules.get(a).unwrap().left.contains(b) {
+                    std::cmp::Ordering::Less
+                } else {
+                    std::cmp::Ordering::Greater
+                }
+            });
+            sum += pages[pages.len() / 2];
+        }
+    });
+    Some(sum)
 }
 
 #[cfg(test)]
