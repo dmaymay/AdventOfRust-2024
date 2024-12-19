c8b5b2c26323b5f549b8a86b499fb7d8a285d23f
dmaymay <dmayoracurzio@gmail.com>
2024-12-19 15:13:48 +0100

    Solved day 8 part two, easy but not efficient, will come back to it

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -11,12 +11,11 @@ pub fn part_one(input: &str) -> Option<u32> {
         for y in 0..cols {
             let antenna = grid[x][y];
             if antenna != '.' {
-                for r in 0..rows - 1 {
-                    for c in 0..cols - 1 {
+                for r in 0..rows {
+                    for c in 0..cols {
                         if antenna == grid[r][c] && (x, y) != (r, c) {
                             let distance = (x as i32 - r as i32, y as i32 - c as i32);
 
-                            // Corrected formulas for antinodes
                             let antinode_one = (x as i32 + distance.0, y as i32 + distance.1);
                             let antinode_two = (r as i32 - distance.0, c as i32 - distance.1);
 
@@ -46,7 +45,66 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+    let grid: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let rows = grid.len();
+    let cols = grid.get(0)?.len();
+
+    for x in 0..rows {
+        for y in 0..cols {
+            let antenna = grid[x][y];
+            if antenna != '.' {
+                for r in 0..rows {
+                    for c in 0..cols {
+                        if (x, y) != (r, c) && antenna == grid[r][c] {
+                        //if antenna == grid[r][c] {
+                            let distance = (x as i32 - r as i32, y as i32 - c as i32);
+                            let mut x_up = x as i32;
+                            let mut y_up = y as i32;
+                            let mut x_down = x as i32;
+                            let mut y_down = y as i32;
+
+                            loop {
+                                x_up += distance.0;
+                                y_up += distance.1;
+
+                                let antinode = (x_up, y_up);
+
+                                if antinode.0 >= 0
+                                    && antinode.0 < rows as i32
+                                    && antinode.1 >= 0
+                                    && antinode.1 < cols as i32
+                                {
+                                    antinodes.insert(antinode);
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            loop {
+                                x_down -= distance.0;
+                                y_down -= distance.1;
+
+                                let antinode = (x_down, y_down);
+
+                                if antinode.0 >= 0
+                                    && antinode.0 < rows as i32
+                                    && antinode.1 >= 0
+                                    && antinode.1 < cols as i32
+                                {
+                                    antinodes.insert(antinode);
+                                } else {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Some(antinodes.len().try_into().unwrap())
 }
 
 #[cfg(test)]
