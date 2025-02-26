b604d19635f43afa7527fdb163512f3ca1a2e0ff
dmaymay <dmayoracurzio@gmail.com>
2025-02-26 15:11:15 +0100

    optimiztion for part one

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -1,44 +1,52 @@
 advent_of_code::solution!(25);
-fn add_vectors(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
-    a.iter().zip(b.iter()).map(|(x, y)| x + y).collect()
+
+fn add_vectors(a: &[u8; 5], b: &[u8; 5]) -> [u8; 5] {
+    let mut result = [0; 5];
+    for i in 0..5 {
+        result[i] = a[i] + b[i];
+    }
+    result
 }
 
-fn is_filled(schematic: &str) -> Vec<u8> {
-    let mut heights: Vec<u8> = vec![0, 0, 0, 0, 0];
+fn is_filled(schematic: &str) -> [u8; 5] {
+    let mut heights = [0; 5];
 
-    schematic.lines().for_each(|l| {
-        let characters: Vec<char> = l.chars().collect();
-        let fill: Vec<u8> = characters
-            .into_iter()
-            .map(|c| if c == '#' { 1 } else { 0 })
-            .collect();
+    for l in schematic.lines() {
+        let mut fill = [0; 5];
+        for (i, c) in l.chars().enumerate().take(5) {
+            if c == '#' {
+                fill[i] = 1;
+            }
+        }
 
-        heights = add_vectors(&heights, &fill);
-    });
+        for i in 0..5 {
+            heights[i] += fill[i];
+        }
+    }
 
-    // Subtract [1, 1, 1, 1, 1] from heights
-    heights.iter_mut().for_each(|h| *h = h.saturating_sub(1));
+    // Subtract [1,1,1,1,1] from heights, ensuring non-negative results
+    for h in &mut heights {
+        *h -= 1;
+    }
 
     heights
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let split: Vec<&str> = input.split("\n\n").collect();
-    let mut keys: Vec<Vec<u8>> = vec![];
-    let mut locks: Vec<Vec<u8>> = vec![];
+    let mut keys: Vec<[u8; 5]> = Vec::new();
+    let mut locks: Vec<[u8; 5]> = Vec::new();
 
     for schematic in split {
         let heights = is_filled(schematic);
-
-        let first_line = schematic.lines().next().unwrap();
-        if first_line == "....." {
-            keys.push(heights)
+        if schematic.lines().next().unwrap() == "....." {
+            keys.push(heights);
         } else {
-            locks.push(heights)
+            locks.push(heights);
         }
     }
-    let mut result = 0;
 
+    let mut result = 0;
     for key in &keys {
         for lock in &locks {
             let combined = add_vectors(key, lock);
@@ -50,7 +58,6 @@ pub fn part_one(input: &str) -> Option<u32> {
 
     Some(result)
 }
-
 pub fn part_two(input: &str) -> Option<u32> {
     None
 }
